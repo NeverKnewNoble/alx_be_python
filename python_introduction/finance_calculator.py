178c844aefdea854ac56cbf65bc7858d41a90773
Nortex <146195144+NeverKnewNoble@users.noreply.github.com>
2024-06-17 23:40:01 +0000

    Create finance_calculator.py

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,15 @@
+# declarations
+annual_int_rate = 5%;
+
+# input for users
+monthly_income = int(input("Enter your monthly income: ")); 
+monthly_expenses = int(input("ENter your total monthly expenses: "));
+
+# Monthly Savings
+monthly_savings = monthly_income - monthly_expenses;
+# Annual Savings
+Annual_savings = monthly_savings * 12 + (monthly_savings * 12 * annual_int_rate);
+
+# Output
+print(f"Your monthly savings are ${montlhy_savings}");
+print(f"Prohject savings after one year, with interest, is: %{Annual_savings}");
