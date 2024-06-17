ca6fb45c4dce7b497c86ad875392d27755ab4ef9
Nortex <146195144+NeverKnewNoble@users.noreply.github.com>
2024-06-17 15:57:36 +0000

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,13 @@
-number1 = 10;
-number2 = 5;
+# Define the two numbers
+number1 = 10
+number2 = 5
 
-Addsum = number1 - number2;
-Subsum = number2 - number1;
-Multisum = number1 * number2;
+# Perform arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
 
-print("Addition" + number1 + "and" + number2 + "is" + Addsum\n"Subtraction" + number2 + "and" + number3 + "is" + Subsum\n"Multiplication" + number1 + "and" + number2 + "is" + Multisum );
+# Print the results
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
