fc14f93c88960f13fd1a7a5f864f0c0519246e81
Nortex <146195144+NeverKnewNoble@users.noreply.github.com>
2024-06-17 17:44:26 +0000

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
 # Define the principal, rate, and time
-principal = 1000
-rate = 0.05
-time = 3
+principal = 1000;
+rate = 0.05;
+time = 3;
 
 # Calculate the simple interest
 interest = principal * rate * time
